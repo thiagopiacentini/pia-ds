5e7906dd14c52285b8d1e31f8897b6c533119227
Thiago Piacentini <thiagopiacentini83@gmail.com>
2023-06-03 20:46:53 +0800

    add button component

---
+++ src/components/PiaButton/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PiaButton = () => {
+  return(
+    <button>
+      clique aqui!
+    </button>
+  )
+}
+
+export default PiaButton;
---
+++ stories/PiaButton.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Meta, Story } from '@storybook/react'
+import PiaButton from "../src/components/PiaButton";
+
+export default {
+   title: 'Components/PiaButton',
+   component: PiaButton
+} as Meta<typeof PiaButton>
+
+const Template: Story<typeof PiaButton> = ()=> <PiaButton/> 
+
+export const Primary = Template.bind({})
